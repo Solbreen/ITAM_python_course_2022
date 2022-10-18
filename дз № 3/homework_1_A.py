25009f938698405e094e86ad1fd129a3ffa7dbfb
Solbreen <113941785+Solbreen@users.noreply.github.com>
2022-10-19 00:15:58 +0300

    Add files via upload

---
+++ дз № 3/homework_1_A.py
@@ -0,0 +1,126 @@
+class Binary:
+    def __init__(self, num):  # инициализация объекта Binary (num - число в 10 сс)
+        self.num = num
+
+    def __add__(self, other):  # сложение объекта Binary и other
+        list1 = list(self.num)
+        list2 = list(other)
+        list3 = []
+        list1.reverse()
+        for i in range(0, len(list1)):
+            list1[i] = int(list1[i])
+        u = 0
+        for i in range(0, len(list1)):
+            u = u + (list1[i] * (2**i))
+        list2.reverse()
+        for i in range(0, len(list2)):
+            list2[i] = int(list2[i])
+        j = 0
+        for i in range(0, len(list2)):
+            j += list2[i] * (2**i)
+        u += j
+        while(u != 0):
+            list3.append(u % 2)
+            u //= 2
+        if(len(list3) == 0):
+            return 0
+        else:
+            list3.reverse()
+            return "".join(map(str, list3))
+
+
+    def __sub__(self, other):  # вычитание other из объекта Binary
+        list1 = list(self.num)
+        list2 = list(other)
+        list3 = []
+        list1.reverse()
+        for i in range(0, len(list1)):
+            list1[i] = int(list1[i])
+        u = 0
+        for i in range(0, len(list1)):
+            u = u + (list1[i] * (2**i))
+        list2.reverse()
+        for i in range(0, len(list2)):
+            list2[i] = int(list2[i])
+        j = 0
+        for i in range(0, len(list2)):
+            j += list2[i] * (2**i)
+        u -= j
+        if u > 0:
+            while(u != 0):
+                list3.append(u % 2)
+                u //= 2
+            list3.reverse()
+            return "".join(map(str, list3))
+        elif u < 0:
+            u = abs(u)
+            while(u != 0):
+               list3.append(u % 2)
+               u //= 2
+            list3.append('-')
+            list3.reverse()
+            return "".join(map(str, list3))
+        elif u == 0:
+            return 0
+
+    def __mul__(self, other):  # умножение объекта Binary на other
+        list1 = list(self.num)
+        list2 = list(other)
+        list3 = []
+        list1.reverse()
+        for i in range(0, len(list1)):
+            list1[i] = int(list1[i])
+        u = 0
+        for i in range(0, len(list1)):
+            u = u + (list1[i] * (2**i))
+        list2.reverse()
+        for i in range(0, len(list2)):
+            list2[i] = int(list2[i])
+        j = 0
+        for i in range(0, len(list2)):
+            j += list2[i] * (2**i)
+        u *= j
+        while(u != 0):
+            list3.append(u % 2)
+            u //= 2
+        if(len(list3) == 0):
+            return 0
+        else:
+            list3.reverse()
+            return "".join(map(str, list3))
+
+    def __floordiv__(self, other):  # целочисленное деление объекта Binary на other
+        list1 = list(self.num)
+        list2 = list(other)
+        list3 = []
+        list1.reverse()
+        for i in range(0, len(list1)):
+            list1[i] = int(list1[i])
+        u = 0
+        for i in range(0, len(list1)):
+            u = u + (list1[i] * (2**i))
+        list2.reverse()
+        for i in range(0, len(list2)):
+            list2[i] = int(list2[i])
+        j = 0
+        for i in range(0, len(list2)):
+            j += list2[i] * (2**i)
+        u //= j
+        while(u != 0):
+            list3.append(u % 2)
+            u //= 2
+        if(len(list3) == 0):
+            return 0
+        else:
+            list3.reverse()
+            return "".join(map(str, list3))
+
+    def __str__(self):  # конвертирование объекта Binary в строку
+        pass
+
+a = Binary(input())
+b = input()
+print(a.__add__(b))
+print(a.__sub__(b))
+print(a.__mul__(b))
+print(a.__floordiv__(b))
